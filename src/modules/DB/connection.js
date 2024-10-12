47f0beb0943482440597f9a9fb194f8f82de2807
Tasbeeh Salahat <tasbeehsa80@gmail.com>
2024-10-12 03:49:43 +0300

    tt

---
+++ src/modules/DB/connection.js
@@ -0,0 +1,21 @@
+// db.js
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+// Use the MongoDB URI from the .env file
+const uri = process.env.MONGODB_URI ; // Default to local if not specified
+
+// Function to connect to MongoDB
+const connectDB = async () => {
+    try {
+        // Remove deprecated options
+        await mongoose.connect(uri);
+        console.log('Connected to MongoDB done ');
+    } catch (err) {
+        console.error('MongoDB connection error:', err);
+        process.exit(1); // Exit process with failure
+    }
+};
+
+// Export the connection function
+module.exports = connectDB;
